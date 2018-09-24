0284251328763d1b3cc80c02f1f87c117723363b
presingjos <josephpresing@Josephs-MBP-15.frontierlocal.net>
2018-09-23 23:44:03 -0400

    resized image

--- client/src/components/ParallaxImage.js
+++ client/src/components/ParallaxImage.js
@@ -5,7 +5,7 @@ import 'font-awesome/css/font-awesome.min.css'
 
 class ParallaxImage extends React.Component {
   render () {
-    let imageDir = 'https://calm-spire-19547.herokuapp.com/images/me.jpg'
+    let imageDir = 'https://calm-spire-19547.herokuapp.com/images/me.JPG'
 
     return (
       <div>
